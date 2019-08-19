6fdcc536bf714dcc60bf32e4ddf4ff18f412ea00
mehmet <mehmetcan.c@gmail.com>
2019-08-19 16:26:48 +0300

    diplay_board() func implemented

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,8 +1,26 @@
 # board
+board = [" " for i in range(9)]
+
 # possible moves
+
 # handle sign
 # display board
+
+
+def display_board():
+    """
+            displays the board on the screen
+    """
+
+    print(" " + board[0] + " | " + board[1] + " | " + board[2])
+    print("-----------")
+    print(" " + board[3] + " | " + board[4] + " | " + board[5])
+    print("-----------")
+    print(" " + board[6] + " | " + board[7] + " | " + board[8])
+
 # play game
 # check is player or computer win
 # check is board full
 # handle turn
+
+display_board()
